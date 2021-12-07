a41d4955e3ef3fdf4e3be0bfeef8a7b3d1bb110f
John Bradshaw <john.bradshaw@cinch.co.uk>
2021-12-07 11:28:04 +0000

    refactor

--- src/AdventOfCode/Day7.cs
+++ src/AdventOfCode/Day7.cs
@@ -11,30 +11,21 @@ namespace AdventOfCode2021
         {
             Inputs = input.Split(',').Select(i => int.Parse(i.Trim())).ToList();
         }
-        public int Part1(){
-            var greatestHorizontal = Inputs.Max();
+        
+        private int _calculateCheapest(Func<int,int,int> costCalculation){
             var costs = new List<int>();
-            for (int i = 0; i < greatestHorizontal; i++)
+            for (int i = 0; i < Inputs.Max(); i++)
             {
-                costs.Add(Inputs.Select(input => Math.Abs(i - input)).Sum());
+                costs.Add(Inputs.Select(input => costCalculation(input,i)).Sum());
             }
             var bestPos = costs.IndexOf(costs.Min());
             Console.WriteLine($"The best position is {costs.IndexOf(costs.Min())} it costs {costs.Min()}");
             return costs.Min();
         }
-        public long Part2(){
-           var greatestHorizontal = Inputs.Max();
-            var costs = new List<int>();
-            for (int i = 0; i < greatestHorizontal; i++)
-            {
-                costs.Add(Inputs.Select(input => {
-                    var n = Math.Abs(i - input);
-                    return (n * (n + 1))/2;
-                    }).Sum());
-            }
-            var bestPos = costs.IndexOf(costs.Min());
-            Console.WriteLine($"The best position is {costs.IndexOf(costs.Min())} it costs {costs.Min()}");
-            return costs.Min();
-        }
+        public int Part1() => _calculateCheapest((input, position) => Math.Abs(position - input));
+        public long Part2() => _calculateCheapest((input, position) => {
+                        var n = Math.Abs(position - input);
+                        return (n * (n + 1))/2;
+                    });
     }
 }
